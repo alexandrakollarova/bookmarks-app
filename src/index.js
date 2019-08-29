145daed27b8222c5e561c36da223647f67a7bb42
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-08-29 12:14:16 -0400

    some changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-=======
 import {BrowserRouter} from 'react-router-dom';
->>>>>>> master
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
-=======
     <BrowserRouter>
         <App />
     </BrowserRouter>, 
-    document.getElementById('root'));
->>>>>>> master
+    document.getElementById('root')
+  );
